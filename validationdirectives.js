863f26e02f5fa2432a7a910cb5b535b1e0fcf14d
chandermani <chandermani@outlook.com>
2014-03-09 12:02:59 +0530

    Fixed both validators. and removed extra code.

--- validationdirectives.js
+++ validationdirectives.js
@@ -1,27 +1,3 @@
-var app = angular.module('form-example1', ['validation']);
-
-var INTEGER_REGEXP = /^\-?\d+$/;
-
-app.controller("Controller", function ($scope, Validations) {
-    angular.extend($scope, Validations);
-});
-app.factory('Validations', function ($timeout) {
-    return {
-        disposableMail: function (email) {
-            if (!email) return true;
-            return email.indexOf('@mailinator.com') >= 0 ? false : true;
-        },
-        spamWarning: function (email) {
-            if (!email) return true;
-            return email.indexOf('@hotmail.com') >= 0 ? false : true;
-        },
-        duplicateMailRemoteCheck: function (email) {
-            return $timeout(function () {
-                return ['mail1@abc.com', 'mail2@abc.com', 'mail3@abc.com'].indexOf(email) >= 0 ? true : false;
-            }, 2000);
-        }
-    };
-});
 angular.module('validation', [])
     .directive('customValidator', [function () {
     return {
@@ -56,7 +32,7 @@ angular.module('validation', [])
         restrict: 'A',
         require: 'ngModel',
         link: function (scope, elm, attr, ngModelCtrl) {
-            var validateFunctionNames = attr["validateFunctions"].split(",");
+            var validateFunctionNames = attr["remoteValidateFunctions"].split(",");
             var validatorNames = attr["customRemoteValidator"].split(",");
             ngModelCtrl.$parsers.push(function (value) {
                 angular.forEach(validateFunctionNames, function (functionName, index) {
@@ -102,7 +78,7 @@ angular.module('validation', [])
         template: '<div><small class="error" ng-repeat="message in errorMessages" ng-show= "!modelController.$pristine && $first" class="warning">{{message}}</small></div>'
     }
 })
-    .directive('ngUpdateOnBlur', function () {
+    .directive('updateOnBlur', function () {
     return {
         restrict: 'A',
         require: 'ngModel',
